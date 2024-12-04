f1fe3e5ea8327f6e82eb397c4a7c776c2d4e4f66
venusai <venusaiyalamanchili@gmail.com>
2024-12-04 20:44:14 +0530

    feat: using groq-llama3

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,41 +1,47 @@
 require("dotenv").config();
 import express from "express";
-import { BASE_PROMPT, getSystemPrompt } from "./prompts";
-import { basePrompt as nodeBasePrompt } from "./defaults/node";
-import { basePrompt as reactBasePrompt } from "./defaults/react";
-import OpenAI from "openai";
+import Groq from "groq-sdk";
 import cors from "cors";
+import { BASE_PROMPT, getSystemPrompt } from "./prompts";
+import { basePrompt as reactBasePrompt } from "./defaults/react";
+import { basePrompt as nodeBasePrompt } from "./defaults/node";
+
+// hit the template route to get the template --- node or react
+// hit the chat route to get the chat  streamline it
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-const anthropic = new OpenAI({
-  baseURL: "https://openrouter.ai/api/v1",
-  apiKey: process.env.ANTHROPIC_API_KEY,
-});
 
+//first route
 app.post("/template", async (req, res) => {
   const prompt = req.body.prompt;
-
-  const response = await anthropic.chat.completions.create({
+  const response = await groq.chat.completions.create({
     messages: [
+      {
+        role: "system",
+        content:
+          "Respond with a single word, either node or 'react', indicating the technology that should be used for this project.",
+      },
       {
         role: "user",
         content: prompt,
       },
-      {
-        role: "system",
-        content:
-          "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra",
-      },
     ],
-    model: "anthropic/claude-3.5-haiku:beta",
-    max_tokens: 200,
+    model: "llama3-70b-8192",
+    max_tokens: 20,
+    temperature: 0.0000001,
   });
-
-  const answer = response.choices[0].message.content?.toLowerCase(); // react or node
-  console.log(answer);
-  if (answer == "react") {
+  console.log(response.choices[0].message);
+  const ans = response.choices[0].message.content
+    ?.toLowerCase()
+    .trim()
+    .split(" ")[0]
+    .replace(/[^a-z]/g, ""); // node or react
+  console.log(ans);
+  if (ans === "react") {
     res.json({
       prompts: [
         BASE_PROMPT,
@@ -43,43 +49,57 @@ app.post("/template", async (req, res) => {
       ],
       uiPrompts: [reactBasePrompt],
     });
+
     return;
   }
 
-  if (answer === "node") {
+  if (ans === "nodejs" || ans === "node") {
     res.json({
       prompts: [
         `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${nodeBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
       ],
       uiPrompts: [nodeBasePrompt],
     });
+
     return;
   }
+  /// nothing works
 
-  res.status(403).json({ message: "You cant access this" });
+  res.status(403).json({
+    message: "You can't do that",
+  });
+
   return;
 });
 
 app.post("/chat", async (req, res) => {
   const messages = req.body.messages;
-  const response = await anthropic.chat.completions.create({
+  const systemprompt = getSystemPrompt();
+  const responseStream = await groq.chat.completions.create({
     messages: [
       {
         role: "system",
-        content: getSystemPrompt(),
+        content: systemprompt,
       },
       ...messages,
     ],
-    model: "anthropic/claude-3.5-haiku:beta",
-    max_tokens: 7000,
+    model: "llama3-70b-8192",
+    max_tokens: 8000,
+    stream: true,
   });
-
-  //console.log(response);
-  //console.log(response.choices[0].message.content);
+  const responseChunks = [];
+  for await (const chunk of responseStream) {
+    responseChunks.push(chunk);
+  }
+  const responseBody = responseChunks
+    .flatMap((chunk) => chunk.choices.map((choice) => choice.delta.content))
+    .join("");
 
   res.json({
-    response: response.choices[0].message.content,
+    response: responseBody,
   });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
